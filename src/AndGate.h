c78c16d6b4c30aafd41918a6cad67af8219a03fa
Kamil Kowalski <kamil.k.kowalski@gmail.com>
2026-08-07 14:25:42 +0200

    Phase 3: The Event-Driven Simulation Engine (#3)

--- src/AndGate.h
+++ src/AndGate.h
@@ -1,16 +1,22 @@
 #pragma once
 
 #include <memory>
+#include <cstdint>
 #include "Gate.h"
 #include "Wire.h"
 
+// Forward declaration to avoid circular dependencies
+class Simulator; 
+
 class AndGate : public Gate {
 public:
-    AndGate(std::shared_ptr<Wire> in1, std::shared_ptr<Wire> in2, std::shared_ptr<Wire> out);
+    AndGate(Simulator& sim, std::shared_ptr<Wire> in1, std::shared_ptr<Wire> in2, std::shared_ptr<Wire> out, uint64_t delay = 5);
     void evaluate() override;
 
 private:
+    Simulator& sim;
     std::shared_ptr<Wire> input1;
     std::shared_ptr<Wire> input2;
     std::shared_ptr<Wire> output;
+    uint64_t delay;
 };
